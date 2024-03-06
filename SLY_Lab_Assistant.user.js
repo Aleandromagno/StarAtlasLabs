82585cb740ed6ff08bc183b3f06eb3f665c9deda
Aleandromagno <159634494+Aleandromagno@users.noreply.github.com>
2024-03-06 07:04:27 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -755,7 +755,7 @@
             let txHash = response.txHash;
             let confirmation = response.confirmation;
             let txResult = await solanaConnection.getTransaction(txHash, {commitment: 'confirmed', preflightCommitment: 'confirmed', maxSupportedTransactionVersion: 1});
-            /*//let priorityHistory = await solanaConnection.getRecentPrioritizationFees();
+            /*let priorityHistory = await solanaConnection.getRecentPrioritizationFees();
             //console.log('priorityHistory: ', priorityHistory);*/
 	    if (confirmation.name == 'TransactionExpiredBlockheightExceededError' && !txResult) {
                 console.log('-----RETRY-----');
